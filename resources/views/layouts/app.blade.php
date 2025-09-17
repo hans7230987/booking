5696761490668329ce1bf8e5f2a3506c1452fdc2
rush55 <hans7230987@gmail.com>
2025-09-17 14:30:23 +0800

    更改style路徑

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>球館通</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -24,15 +24,9 @@
                 <a href="{{ route('login') }}">登入</a>&nbsp; |
                 <a href="{{ route('register') }}">註冊</a>
                 @else
-<<<<<<< HEAD
                 <span>{{ Auth::user()->name }} 您好</span> |
                 <a href="{{ route('home') }}">首頁</a> |
                 <a href="{{ route('profile.edit') }}">修改會員資訊</a> |
-=======
-                <span>{{ Auth::user()->name }} 您好</span>&nbsp; |
-                <a href="{{ route('home') }}">首頁</a>&nbsp; |
-                <a href="#">修改會員資訊</a>&nbsp; |
->>>>>>> ff42174354c9e5c708d98fe9f385baabdd20d78e
                 <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     登出
                 </a>
